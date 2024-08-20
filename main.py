c7124c0ad3d42ccd33b56741a316461cdeebc446
AERO-h <semenzaharev526@gmail.com>
2024-08-21 06:32:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def single_root_words(root_word, *other_words):
+    root_word = root_word.lower()
     same_words = []
     for i in other_words:
+        i = i.lower()
         if root_word in i or i in root_word:
             same_words.append(i)
 
